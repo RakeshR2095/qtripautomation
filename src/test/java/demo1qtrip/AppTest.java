322a2e91aa053c2b565fd1781eca7ba919f93b4c
@rakeshr2095 <rakeshr2095@gmail.com>
2024-05-21 12:29:47 +0530

    First Commit

---
+++ src/test/java/demo1qtrip/AppTest.java
@@ -0,0 +1,20 @@
+package demo1qtrip;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
